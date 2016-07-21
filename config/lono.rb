69d60db62f3ff81e93437b0f242fe8fa4b087f03
Ivan Fetch <ivan.fetch@gmail.com>
2016-07-20 20:08:27 -0600

    Initial commit of test templates

---
+++ config/lono.rb
@@ -0,0 +1,22 @@
+template "stack.json" do
+  source "stack.json.erb"
+variables(
+  instances: [{
+  "app1": {
+    description: "tomcat",
+    HostName: "lad01",
+    role: "App",
+    AZ: "A",
+    RootVolumeSize: "8",
+    type: "t2.micro",
+    InstanceProfile: "Ec2-Role",
+    AdditionalVolumes: [
+      device: "/dev/xvdb",
+         size: 10,
+         mount_point: "/data/local/tomcat"
+  ]
+  },
+}]
+)
+
+end
