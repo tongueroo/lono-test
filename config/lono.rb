09b519644656d163b0d879af4eb54decccf7d087
Tung Nguyen <tongueroo@gmail.com>
2016-07-21 10:28:24 -0700

    fix instance.json.erb: change to instance variable @instance

--- config/lono.rb
+++ config/lono.rb
@@ -1,22 +1,21 @@
 template "stack.json" do
   source "stack.json.erb"
-variables(
-  instances: [{
-  "app1": {
-    description: "tomcat",
-    HostName: "lad01",
-    role: "App",
-    AZ: "A",
-    RootVolumeSize: "8",
-    type: "t2.micro",
-    InstanceProfile: "Ec2-Role",
-    AdditionalVolumes: [
-      device: "/dev/xvdb",
-         size: 10,
-         mount_point: "/data/local/tomcat"
-  ]
-  },
-}]
-)
-
+  variables(
+    instances: [{
+    "app1": {
+      description: "tomcat",
+      HostName: "lad01",
+      role: "App",
+      AZ: "A",
+      RootVolumeSize: "8",
+      type: "t2.micro",
+      InstanceProfile: "Ec2-Role",
+      AdditionalVolumes: [
+        device: "/dev/xvdb",
+           size: 10,
+           mount_point: "/data/local/tomcat"
+      ]
+      },
+    }]
+  )
 end
